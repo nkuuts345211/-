27db4560ca60b609a575a07b3704d149dd5e12b7
nkuuts345211 <aistu112534@gmail.com>
2025-03-17 16:12:58 +0800

    Delete --main directory

--- --main/del menu.php
+++
@@ -1,7 +0,0 @@
-<?php
-include("db.php");
-$id=$_GET["id"];
-$sql="DELETE FROM `food` WHERE `id` = $id";
-mysqli_query($link,$sql);
-echo "<script>location.href='menu user a.php'</script>";
-?>
--- --main/del.php
+++
@@ -1,7 +0,0 @@
-<?php
-include("db.php");
-$id=$_GET["id"];
-$sql="DELETE FROM `msg` WHERE `id` = $id";
-mysqli_query($link,$sql);
-echo "<script>location.href='u_main.php'</script>";
-?>
