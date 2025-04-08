d4a81ab5a786b962b71dbb177cb5e9ac70bf88f9
nkuuts345211 <aistu112534@gmail.com>
2025-04-08 11:46:00 +0800

    Add files via upload

--- index food.php
+++ index food.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>菜單</title>
-        <link rel="stylesheet" href="style/style food.css">
+        <link rel="stylesheet" href="style/food.css">
     </head>
     <body>
         <table>
--- index user u food.php
+++ index user u food.php
@@ -4,13 +4,10 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>菜單</title>
-    </head>
-    <body align="center">
         <link rel="stylesheet" href="style/food.css">
+    </head>
+    <body>
         <table>
-            <tr class="header" style="color:white;" >
-                <td><h1>美味時光 (Flavor Time)</h1></td>
-            </tr>
             <tr>
                 <td class="header">
                     <table>
@@ -30,17 +27,15 @@
             </tr>
             <tr>
                 <td class="content"><table>
-                    <tr><td><img src="\img\menu1.jpg"><p>菜單介紹</p><p>價格<br>900</p></td>
-                    <td><img src="\img\menu2.jpg"><p>菜單介紹</p><p>價格<br>700</p></td>
-                    <td><img src="\img\menu3.jpg"><p>菜單介紹</p><p>價格<br>800</p></td>
-                    <td><img src="\img\menu4.jpg"><p>菜單介紹</p><p>價格<br>500</p></td>
-                    <td><img src="\img\menu5.jpg"><p>菜單介紹</p><p>價格<br>300</p></td>
+                    <tr><td><img src="\img\menu1.jpg"><p>菜單介紹</p><p>價格</p></td>
+                    <td><img src="\img\menu2.jpg"><p>菜單介紹</p><p>價格</p></td>
+                    <td><img src="\img\menu3.jpg"><p>菜單介紹</p><p>價格</p></td>
+                    <td><img src="\img\menu4.jpg"><p>菜單介紹</p><p>價格</p></td>
+                    <td><img src="\img\menu5.jpg"><p>菜單介紹</p><p>價格</p></td>
                 </tr>
                 </table></td>
             </tr>
-            
             <tr>
-    
                 <td class="footer">
                     <table>
                         <tr>
