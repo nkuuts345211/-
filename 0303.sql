17c0e5424963514801353fb9c95e1197ab96c934
nkuuts345211 <aistu112534@gmail.com>
2025-04-17 13:04:52 +0800

    Add files via upload

--- 0303.sql
+++ 0303.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2025-04-08 05:41:05
+-- 產生時間： 2025-04-17 07:00:32
 -- 伺服器版本： 10.4.32-MariaDB
 -- PHP 版本： 8.0.30
 
@@ -32,9 +32,22 @@ CREATE TABLE `food` (
   `c_num` varchar(20) NOT NULL,
   `c_name` varchar(20) NOT NULL,
   `c_money` int(11) NOT NULL,
-  `img` int(11) NOT NULL,
-  `add_time` datetime NOT NULL
+  `img` text NOT NULL,
+  `add_time` datetime NOT NULL,
+  `text` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- 傾印資料表的資料 `food`
+--
+
+INSERT INTO `food` (`id`, `c_num`, `c_name`, `c_money`, `img`, `add_time`, `text`) VALUES
+(1, 'A001', 'menu1', 1000, 'menu1.jpg', '2025-04-16 15:04:38', '香嫩多汁大雞排'),
+(2, 'A002', 'menu2', 1000, 'menu2.jpg', '2025-04-16 15:07:40', '超級美式熱狗堡'),
+(3, 'A003', 'menu3', 2000, 'menu3.jpg', '2025-04-17 11:31:58', '大烤雞全餐'),
+(4, 'A004', 'menu4', 2000, 'menu4.jpg', '2025-04-17 12:56:11', '色香味俱全的烤雞腿'),
+(5, 'A005', 'menu5', 1000, 'menu5.jpg', '2025-04-17 12:58:13', '美味的焗烤麵'),
+(6, 'A006', 'menu6', 1000, 'menu6.jpg', '2025-04-17 12:58:49', '金黃酥脆的炸薯條');
 
 -- --------------------------------------------------------
 
@@ -85,11 +98,11 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `account`, `password`, `name`, `type`) VALUES
-(5, '1212', '1212', '管理者', 'a'),
-(6, '1111', '1111', '使用者1', 'u'),
-(7, '1234', '1234', '賣方', 'o'),
-(12, '456', '456', '使用者2', 'u'),
-(14, '傘套', '111', '楊恩凱', 'u');
+(1, '1212', '1212', '管理者', 'a'),
+(2, '1111', '1111', '使用者1', 'u'),
+(3, '1234', '1234', '賣方', 'o'),
+(4, '456', '456', '使用者2', 'u'),
+(5, '傘套', '111', '楊恩凱', 'u');
 
 --
 -- 已傾印資料表的索引
@@ -127,13 +140,13 @@ ALTER TABLE `user`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `food`
 --
 ALTER TABLE `food`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `msg`
 --
 ALTER TABLE `msg`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `order`
@@ -145,7 +158,7 @@ ALTER TABLE `order`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
