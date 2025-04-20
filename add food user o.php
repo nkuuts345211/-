22cd073f48756ef7667e99022458f0a075de30f7
nkuuts345211 <aistu112534@gmail.com>
2025-04-20 13:57:12 +0800

    Add files via upload

--- add food user o.php
+++ add food user o.php
@@ -6,40 +6,7 @@
                 $res=mysqli_query($link,$sql);
                 if(mysqli_num_rows($res)>0){
                     while($row=mysqli_fetch_assoc($res)){
-                        echo "<form action='add%20food%20user.php' method='post'>";
-                        echo "<table align='center' border='1px solid black'>";
-                        echo "<tr>";
-                        echo "<td>";
-                        echo "品名 : ";
-                        echo "</td>";
-                        echo "<td>";
-                        echo "<input type='text' name='c_name' value='".$row["c_name"]."'>";
-                        echo "</td>";
-                        echo "</tr>";
-                        echo "<tr>";
-                        echo "<td>";
-                        echo "價格 : ";
-                        echo "</td>";
-                        echo "<td>";
-                        echo "<input type='text' name='c_money' value='".$row["c_money"]."'>";
-                        echo "</td>";
-                        echo "</tr>";
                         
-                        echo "<tr>";
-                        echo "<td>";
-                        echo "介紹 : ";
-                        echo "</td>";
-                        echo "<td>";
-                        echo "<input type='text' name='text' value='".$row["text"]."'>";
-                        echo "</td>";
-                        echo "</tr>";
-                        echo "<tr>";
-                        echo "<td colspan='2'>";
-                        echo "<input type='submit'  value='送出'>";
-                        echo "</td>";
-                        echo "</tr>";
-                        echo "</table>";
-                        echo "<input type='hidden' name='id' value='".$row["id"]."'>";
 
                     }
                 }
--- add food user.php
+++ add food user.php
@@ -3,11 +3,15 @@ include("db.php");
 $c_name=$_POST["c_name"];
 $c_money=$_POST["c_money"];
 $text=$_POST["text"];
+$img=$_POST["img"];
+$c_num=$_POST["c_num"];
 $account=$_SESSION["account"];
 
 $id=$_POST["id"];
+$sql="DELETE FROM `food` WHERE `id`=$id";
+mysqli_query($link,$sql);
  
-$sql="UPDATE `food` SET `c_name`='$c_name',`c_money`='$c_money',`text`='$text' WHERE `id`=$id";
+$sql="INSERT INTO `food`(`id`, `c_num`, `c_name`, `c_money`, `img`, `add_time`, `text`) VALUES ('$id','$c_num','$c_name','$c_money','$img',NOW(),'$text')";
     mysqli_query($link,$sql);
       $name="SELECT * FROM `user` WHERE `account`='$account'";
         $add=mysqli_query($link,$name);
