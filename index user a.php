4aabd917cd65ef156a4a3ea11bb2626b7fae44da
nkuuts345211 <aistu112534@gmail.com>
2025-05-05 16:35:26 +0800

    Add files via upload

--- index user a.php
+++ index user a.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>管理者首頁</title>
-        <link rel="stylesheet" href="style/user a.css">
+        <link rel="stylesheet" href="style/user_a.css">
     </head>
     <body>
         <table>
--- index user o.php
+++ index user o.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>賣家首頁</title>
-        <link rel="stylesheet" href="style/user a.css">
+        <link rel="stylesheet" href="style/user_a.css">
     </head>
     <body>
         <table>
