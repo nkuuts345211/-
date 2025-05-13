0039cb10272f82397403d9f690663a06de4db068
nkuuts345211 <aistu112534@gmail.com>
2025-05-13 10:30:55 +0800

    Add files via upload

--- index user a food.php
+++ index user a food.php
@@ -47,19 +47,22 @@
                 $res=mysqli_query($link,$sql);
                 if(mysqli_num_rows($res)>0){
                     while($row=mysqli_fetch_assoc($res)){
+                        echo "<form action='orderadd.php' method='post'>";
                         echo "<table style='width:700px' border='2' align='center'>";
                         echo "<tr style='height:50px'>";
                         echo "<td colspan='2'>".'品名:'.$row["c_name"]."</td>";
-                        
                         echo "</tr>";
-                        echo "<tr style='height:400px'><td colspan='2'>"."<img src='img/".$row['img']."' style='width:300px'><br>".$row["text"]."</td></tr>";
+                        echo "<tr style='height:400px'><td colspan='2'><img src='img/".$row['img']."' style='width:300px'><br>".$row["text"]."</td></tr>";
                         echo "<tr style='height:50px'>";
-                        echo "<td>".'$$:'.$row["c_money"]."</td>";
-                        echo "<td><input type='button' value='123' onclick=location.href='orderadd.php?id=".$row["id"]."'></td>";
+                        echo "<td>$$:".$row["c_money"]."</td>";
+                        echo "<td>";
+                        echo "數量: <input type='number' name='buy_amount' value='1' min='1' required>";
+                        echo "<input type='hidden' name='id' value='".$row["id"]."'>";
+                        echo "<input type='submit' value='加入訂單'>";
+                        echo "</td>";
                         echo "</tr>";
-                        echo "<br>";
                         echo "</table>";
-                        
+                        echo "</form><br>";
                     }
                 }
             //    echo  "<td><img src='\img\menu1.jpg'><p>菜單介紹</p><p>價格</p></td>";
