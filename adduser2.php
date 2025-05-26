1dabe5a5887304638ee0c0f97324c21d4d67c118
nkuuts345211 <aistu112534@gmail.com>
2025-05-26 17:02:14 +0800

    Add files via upload

--- adduser2.php
+++ adduser2.php
@@ -3,9 +3,17 @@ include("db.php");
 $account=$_POST["account"];
 $password=$_POST["password"];
 $name=$_POST["name"];
+$p=0;
+$check="SELECT * FROM `user` WHERE `account`=$account";
+$check_res=mysqli_query($link,$check);
+if(mysqli_num_rows($check_res)>0){
+    echo "<script>alert('已有此帳號')</script>";
+    echo "<script>location.href='add%20user.php'</script>";
+}else{
+    $sql="INSERT INTO `user`(`id`, `account`, `password`, `name`, `type`) VALUES (null,'$account','$password','$name','u')";
+    mysqli_query($link,$sql);
+    echo "<script>alert('註冊成功，請重新登入')</script>";
+    echo "<script>location.href='index%20login.php'</script>";
+}
 
-$sql="INSERT INTO `user`(`id`, `account`, `password`, `name`, `type`) VALUES (NULL,'$account','$password','$name','u')";
-$res=mysqli_query($link,$sql);
-echo "<script>alert('註冊成功，請重新登錄')</script>";
-echo "<script>location.href='index.php'</script>";
 ?>
