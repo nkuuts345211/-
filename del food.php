a2ae4090502940ec1187149b4100866ccf2759a5
nkuuts345211 <aistu112534@gmail.com>
2025-06-03 10:58:15 +0800

    Add files via upload

--- del food.php
+++ del food.php
@@ -1,11 +1,14 @@
 <?php
 include("db.php");
 $id=$_GET["id"];
+
 $sql="DELETE FROM `food` WHERE `id` = $id";
 mysqli_query($link,$sql);
-
+if($_SESSION["type"]=="o"){
         header("location:menu%20user%20o.php");
-
+}else{
+        header("location:menu%20user%20a.php");
+}
 
 
 ?>
